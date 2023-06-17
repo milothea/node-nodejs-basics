20c5a28b0b5fe005a4eba688e42780cccc2637be
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-06-17 13:33:29 +0200

    feat: implement file creating logic

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import * as fs from 'node:fs';
 const create = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fresh.txt';
+    const content = 'I am fresh and young';
+    const fileExists = fs.existsSync(filePath);
+
+    if (!fileExists) {
+        fs.appendFileSync(filePath, content, (err) => console.error(err));
+
+        return 'file is created';
+    }
+
+    throw new Error('FS operation failed');
 };
 
 await create();
