e11ca90cd78321923a26ab57e30759218146926d
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-06-17 18:33:18 +0200

    feat: implement file reading with stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream('src/streams/files/fileToRead.txt');
+
+    stream.on('data', (chunk) => stdout.write(chunk.toString()))
 };
 
 await read();
