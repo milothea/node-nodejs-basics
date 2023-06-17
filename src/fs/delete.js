ef17a161b504923117fca71c484cf62dcd60af28
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-06-17 13:45:42 +0200

    feat: implement file removing

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import * as fs from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fileToRemove.txt';
+
+    if(fs.existsSync(filePath)) {
+        fs.rmSync(filePath);
+
+        return 'File has been removed';
+    }
+
+    throw new Error('FS operation failed');
 };
 
 await remove();
