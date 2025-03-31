b4eb57584a7869d719c673bd2988a8c511c947e2
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2025-03-31 19:49:05 +0200

    feat: resolve tasks to working with node child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import { spawn } from 'node:child_process';
+import { stdin, stdout } from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = new URL('./files/script.js', import.meta.url);
+
+    for (let i = 0; i < args?.length; i++) {
+        const childProcess = spawn('node', [scriptPath.pathname, args[i]]);
+
+        stdin.pipe(childProcess.stdin);
+        childProcess.stdout.pipe(stdout);
+        childProcess.on('error', (err) => {
+            stdout.write(`Something went wrong with the following error: ${err.message || err}`);
+        });
+    }
 };
 
 // Put your arguments in function call to test this functionality
