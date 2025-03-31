ff4a7d582fd4c5eea10119ec744ec412f2930a66
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2025-03-31 13:45:10 +0200

    feat: resolve tasks to working with ES modules

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,21 +1,30 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { dirname, sep } from 'node:path';
+import { readFile } from 'node:fs';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import './files/c.cjs';
 
 const random = Math.random();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 let unknownObject;
 
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
+async function loadJSON(num) {
+    const filePath = num > 0.5 ? './files/a.json' : './files/b.json';
+
+    try {
+        unknownObject = await readFile(filePath);
+    } catch (err) {
+        console.log(`Something went wrong with the following error: ${err.message || err}`);
+    }
 }
+
+loadJSON(random);
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
-console.log(`Path segment separator is "${path.sep}"`);
+console.log(`Path segment separator is "${sep}"`);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
@@ -33,8 +42,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
-
