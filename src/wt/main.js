b8edd8eface2ef1ba93e51c714242d14634b922b
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2025-03-31 19:13:34 +0200

    feat: resolve tasks to working with node workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { cpus } from 'node:os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpusNumber = cpus().length;
+    const startInput = 10;
+    const workers = [];
+
+    for (let i = 0; i < cpusNumber; i++) {
+        workers.push(new Promise((resolve) => {
+            const worker = new Worker(new URL('./worker.js', import.meta.url));
+            const input = startInput + i;
+
+            worker.postMessage(input);
+            worker.on('message', (res) => {
+                resolve({
+                    status: 'resolved',
+                    data: res,
+                });
+            });
+            worker.on('error', () => {
+                resolve({
+                    status: 'error',
+                    data: null,
+                });
+            });
+            worker.on('exit', (code) => console.log(code))
+        }))
+    }
+
+    const result = await Promise.all(workers);
+
+    console.log(result);
 };
 
 await performCalculations();
