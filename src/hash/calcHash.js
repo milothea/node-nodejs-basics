fba3e06ea068fca73814020c2e46054b7cfc351a
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2025-03-31 13:45:37 +0200

    feat: resolve tasks to working with hashing in node

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const stream = createReadStream('src/hash/calcHash.js');
+
+    await pipeline(stream, hash);
+
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
